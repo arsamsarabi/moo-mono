e1483e871c16c0320b91cd721620ebb1e5c4e203
Arsam <arsamsarabi@me.com>
2021-05-11 13:46:36 +0100

    Adds MediaQuery breakpoints

---
+++ moo-react-components/src/theme/mediaQueries.ts
@@ -0,0 +1,13 @@
+const size = {
+  largeMobile: '414px',
+  tablet: '834px',
+  laptop: '1024px',
+  desktop: '2560px',
+};
+
+export const devices = {
+  largeMobile: `(min-width: ${size.largeMobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
